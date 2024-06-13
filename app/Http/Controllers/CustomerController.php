2b1508ea83b1be0b2949f8e5a31a79c9ae3b5ab2
anas <o@gamil.com>
2024-06-13 15:41:00 +0300

    Optimize customer check and update/create process with Redis caching

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -12,7 +12,7 @@ class CustomerController extends Controller
 {
     public function checkCustomer(Request $request)
     {
-        $customer_id = Customer::where('national_id', $request->national_id)->first(); // take 2.85 seconds
+        $customer_id = Customer::where('national_id', $request->national_id)->first(); //take 2.85 seconds
 
         // $customer_id = Redis::get('national_' . $request->national_id); // but this take 287 ms
 
@@ -20,7 +20,7 @@ class CustomerController extends Controller
 
             Customer::where('id', $customer_id)->update($request->all());
         } else {
-            
+
             Customer::create($request->all());
         }
     }
